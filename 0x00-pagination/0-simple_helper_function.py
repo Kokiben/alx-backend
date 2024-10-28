7965e585fddff10f365811c5c643989c98442212
Kokiben <bennassarkaoutar@gmail.com>
2024-10-28 21:38:45 +0000

    helper

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -10,8 +10,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         page (int): The page number (1-indexed).
         page_size (int): The number of items per page.
     Returns:
-        Tuple[int, int]:tuple containing first index and last index for requested page.
+        Tuple[int, int]:tuple containing first ind and last ind for requested.
     """
-    first_index = (page - 1) * page_size  # Calculate first index for the given page
-    last_index = first_index + page_size  # Calculate last index based on first index
+    first_index = (page - 1) * page_size  # Calcul first ind for given page
+    last_index = first_index + page_size  # Calcul last ind based on first ind
     return first_index, last_index
