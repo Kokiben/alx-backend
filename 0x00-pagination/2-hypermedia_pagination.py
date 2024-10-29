cd5f912fa69f2da36b7f902c64741a2027b772dc
Kokiben <128861864+Kokiben@users.noreply.github.com>
2024-10-29 04:17:00 +0100

    Update 2-hypermedia_pagination.py

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -85,4 +85,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     Returns:
         Tuple[int, int]: A tuple containing the start and end indexes.
     """
-    return (page - 1) * page_size, page * page_siz
+    return (page - 1) * page_size, page * page_size
