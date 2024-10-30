8c68b3a6e0e9cd09ac61fb2c7fd70b6ad379f896
Kokiben <bennassarkaoutar@gmail.com>
2024-10-30 23:53:02 +0000

    fifo

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -10,35 +10,38 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """Initialize cache"""
         super().__init__()
-        self.order = []  # List to track the order of keys for FIFO eviction
+        self.c_d = self.cache_data
+        self.order = []  # List to track order of keys for FIFO eviction
 
     def put(self, key, item):
-        """Add item to the cache_data dictionary with FIFO eviction policy.
+        """Add item to the c_d dictionary with FIFO eviction policy.
+
         Args:
             key (str): The key for the item.
             item (Any): The item to store.
+
         If key or item is None, this method does nothing.
-        If the cache exceeds BaseCaching.MAX_ITEMS, the oldest item is discarded.
+        If cache exceeds BaseCaching.MAX_ITEMS,oldest item is discarded.
         """
         if key is not None and item is not None:
-            # If key is new and cache is at max capacity, discard oldest entry
-            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                oldest_key = self.order.pop(0)  # Get and remove the first inserted key
-                del self.cache_data[oldest_key]
+            # If key is new cache is at max capacity, discard oldest entry
+            if key not in self.c_d and len(self.c_d) >= BaseCaching.MAX_ITEMS:
+                oldest_key = self.order.pop(0)  # Get and remove
+                del self.c_d[oldest_key]
                 print(f"DISCARD: {oldest_key}")
 
-            # Add/Update the item in cache_data and update the order list
-            if key in self.cache_data:
-                self.order.remove(key)  # Remove key if it already exists to update position
-            self.cache_data[key] = item
+            # Add/Update the item in c_d and update the order list
+            if key in self.c_d:
+                self.order.remove(key)  # Remove
+            self.c_d[key] = item
             self.order.append(key)
 
     def get(self, key):
-        """Retrieve item by key from cache_data dictionary.
+        """Retrieve item by key from c_d dictionary.
         Args:
             key (str): The key of the item to retrieve.
         Returns:
-            Any: The value in cache_data linked to key, or None if key is None
-                 or key doesn't exist in cache_data.
+            Any: The value in c_d linked to key, or None if key is None
+                 or key doesn't exist in c_d.
         """
-        return self.cache_data.get(key, None)
+        return self.c_d.get(key, None)
