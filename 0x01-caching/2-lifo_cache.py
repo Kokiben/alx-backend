c357e0b0288eab40b9992843cabec2e57454eef7
Kokiben <bennassarkaoutar@gmail.com>
2024-10-30 23:45:33 +0000

    lifo

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -11,7 +11,7 @@ class LIFOCache(BaseCaching):
         """Initialize cache"""
         super().__init__()
         self.c_d = self.cache_data
-        self.order = []  # List to track 
+        self.order = []  # List to tak
 
     def put(self, key, item):
         """Add item to the c_d dictionary with LIFO eviction policy.
@@ -24,7 +24,7 @@ class LIFOCache(BaseCaching):
         if key is not None and item is not None:
             # If key is new and cache is at max capacity
             if key not in self.c_d and len(self.c_d) >= BaseCaching.MAX_ITEMS:
-                last_key = self.order.pop()  # Get and remove 
+                last_key = self.order.pop()
                 del self.c_d[last_key]
                 print(f"DISCARD: {last_key}")
 
