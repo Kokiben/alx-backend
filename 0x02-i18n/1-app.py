f1c1490420349718f03aeb60611518e50571dd04
Kokiben <bennassarkaoutar@gmail.com>
2024-11-05 19:03:04 +0000

    app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel integration.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """App configuration class."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """Render the index page with a welcome message."""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
