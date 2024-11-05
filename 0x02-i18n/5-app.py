6eafa3739fc5d7a100898c470949fea5e31f7011
Kokiben <bennassarkaoutar@gmail.com>
2024-11-05 22:08:37 +0000

    trans

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with a mock user login system.
+"""
+
+from flask import Flask, render_template, g, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+babel = Babel(app)
+
+
+# Mock user table
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user():
+    """Return a user dictionary or None if no user is found."""
+    user_id = request.args.get('login_as')
+    if user_id and user_id.isdigit():
+        user_id = int(user_id)
+        return users.get(user_id)
+    return None
+
+
+@app.before_request
+def before_request():
+    """Set the user globally using get_user."""
+    user = get_user()
+    g.user = user
+
+
+@app.route('/')
+def index():
+    """Render the index page."""
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5001)
